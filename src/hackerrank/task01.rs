b99e3080ae9120f7046a76d141ed6775bf232bc6
Lemonka1 <dashazubik31@gmail.com>
2026-02-24 10:06:43 +0200

    Solved task01 final

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,20 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+#![allow(non_snake_case)]
+
+pub fn simpleArraySum(ar: &[i32]) -> i32 {
+    let mut sum = 0;
+    for element in ar {
+        sum += element;
+    }
+    return sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simpleArraySum() {
+        let ar = [1, 2, 3, 4, 10, 11];
+        assert_eq!(simpleArraySum(&ar), 31);
+    }
 }
